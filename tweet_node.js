ebaab99b1e2b4d8ef9d7293dc37f2ae2ec492c22
osjupiter <water_totuka@yahoo.co.jp>
2016-03-01 18:10:25 +0900

    キーを隠しました

--- tweet_node.js
+++ tweet_node.js
@@ -1,10 +1,10 @@
 var Twitter = require('twitter');
 require('date-utils');
 var client = new Twitter({
-  consumer_key: 'CEFJU443oTCZzSzbUC2s7NoVW',
-  consumer_secret: 'EAY1stJRpcC8YH2HxEbRmLSfNA0WvZm7kwc0o4tLn7D7mNpruz',
-  access_token_key: '3236421059-wpVkCP4Y4VaRYBdZeI8l0NwSjWihtadYkxNhbUL',
-  access_token_secret: '28JVlSxcZbA9Xpdm4OjkOsDpspxaKIXP4TwlXCwAMjk9N'
+  consumer_key: ',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 });
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var dt=new Date();
